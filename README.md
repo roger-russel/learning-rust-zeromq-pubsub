e070bfdd845de2694ed2d34f9af80919b712489e
Roger Russel <nemesis@rrussel.org>
2020-02-08 07:27:23 -0300

    code working

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Rust and ZeroMQ
+
+I did nothing just seen that this guy did http://zsiciarz.github.io/24daysofrust/book/vol1/day20.html
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+
+extern crate zmq;
+
+use zmq::{Context, Message, Error};
+
+#[cfg(target_family="unix")]
+fn main() {
+    println!("24 days of Rust - zmq (day 20)");
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() < 2 {
+        println!("Usage: {} (client|server)", args[0]);
+        return;
+    }
+    let mut ctx = Context::new();
+    let addr = "tcp://127.0.0.1:25933";
+    if args[1] == "client" {
+        println!("ZeroMQ client connecting to {}", addr);
+        run_client(&mut ctx, addr).unwrap_or_else(|err| println!("{:?}", err));
+    } else {
+        println!("ZeroMQ server listening on {}", addr);
+        run_server(&mut ctx, addr).unwrap_or_else(|err| println!("{:?}", err));
+    }
+}
+
+fn run_client(ctx: &mut Context, addr: &str) -> Result<(), Error> {
+    let sock = ctx.socket(zmq::REQ)?;
+    sock.connect(addr)?;
+    let payload = "Hello world!";
+    println!("-> {:?}", payload);
+    let mut msg = Message::new();
+    sock.send(payload.as_bytes(), 0)?;
+    sock.recv(&mut msg, 0)?;
+    let contents = msg.as_str().unwrap();
+    println!("<- {:?}", contents);
+    Ok(())
+}
+
+
+fn run_server(ctx: &mut Context, addr: &str) -> Result<(), Error> {
+    let sock = ctx.socket(zmq::REP)?;
+    sock.bind(addr)?;
+    let mut msg = Message::new();
+    loop {
+        if sock.recv(&mut msg, 0).is_ok() {
+            sock.send(msg.as_str().unwrap().as_bytes(), 0)?;
+        }
+    }
+}
+
+#[cfg(target_family="windows")]
+fn main() {
+    println!("You are supose to be using Unix.");
+}
